b9f206318a65632dc2b7dfedae18b2603071dae8
u10346504 <tpsikhitha@gmail.com>
2014-07-01 12:19:34 +0200

    Added Scenes tables to database

--- implementation/webservice/test.sql
+++ implementation/webservice/test.sql
@@ -100,3 +100,103 @@ create table if not exists scenevVictims
 
 
 select * from users;
+
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 01, 2014 at 01:12 PM
+-- Server version: 5.1.37
+-- PHP Version: 5.3.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `Scenes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aviation`
+--
+
+CREATE TABLE IF NOT EXISTS `aviation` (
+  `Photo` text NOT NULL,
+  `BodyDecompose` text NOT NULL,
+  `MedicalIntervention` text NOT NULL,
+  `BodyBurned` text NOT NULL,
+  `BodyIntact` text NOT NULL,
+  `CloseToWater` text NOT NULL,
+  `AviationOutside` text NOT NULL,
+  `AircraftType` text NOT NULL,
+  `AircraftNumPeople` text NOT NULL,
+  `Victim` text NOT NULL,
+  `VictiomIO` text NOT NULL,
+  `WeatherCondition` text NOT NULL,
+  `WeatherType` text NOT NULL,
+  `SuicideNote` text NOT NULL,
+  `GeneralHistory` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `aviation`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bicycle`
+--
+
+CREATE TABLE IF NOT EXISTS `bicycle` (
+  `BicyclePhoto` text NOT NULL,
+  `BicycleBodyDecompose` varchar(5) NOT NULL,
+  `BicycleMedicalIntervention` varchar(5) NOT NULL,
+  `BicycleFoundBoby` text NOT NULL,
+  `BicycleBodyIntact` varchar(5) NOT NULL,
+  `BicycleOutside` text NOT NULL,
+  `BicycleNumPeople` text NOT NULL,
+  `BicycleHit` text NOT NULL,
+  `BicycleType` text NOT NULL,
+  `BicycleWeatherCondition` text NOT NULL,
+  `BicycleSuicideNote` varchar(5) NOT NULL,
+  `BicycleGeneralHistory` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bicycle`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blunt`
+--
+
+CREATE TABLE IF NOT EXISTS `blunt` (
+  `BluntPhoto` text NOT NULL,
+  `BluntBodyDecompose` varchar(5) NOT NULL,
+  `BluntMedicalIntervention` varchar(5) NOT NULL,
+  `BluntFoundBody` text NOT NULL,
+  `BluntCloseWater` varchar(5) NOT NULL,
+  `BluntRapeHomicide` varchar(5) NOT NULL,
+  `BluntSuspicionSuicide` varchar(5) NOT NULL,
+  `BluntPreviousTempts` varchar(5) NOT NULL,
+  `BluntIO` text NOT NULL,
+  `BluntForceObjectSuspected` text NOT NULL,
+  `BluntForceObject` varchar(5) NOT NULL,
+  `BluntSuspicionStangulation` varchar(5) NOT NULL,
+  `BluntSuspicionSmothering` varchar(5) NOT NULL,
+  `BluntSuspicionChocking` varchar(5) NOT NULL,
+  `BluntSuicideNote` varchar(5) NOT NULL,
+  `BluntGeneralHistory` varchar(5) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `blunt`
+--
+
