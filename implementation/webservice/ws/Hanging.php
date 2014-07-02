ebda9ad5f5b6f8ce66b4b916087f65d856276ca1
CollenMphabantshi <mickeybanchi@gmail.com>
2014-07-02 12:11:19 +0200

    added classes

---
+++ implementation/webservice/ws/Hanging.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Hanging
+ *
+ * @author BANCHI
+ */
+class Hanging extends Scene{
+    //put your code here
+}
