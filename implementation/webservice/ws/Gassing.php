4820d6828b12b4b5585c73e1c9423c1856b9d1a1
CollenMphabantshi <mickeybanchi@gmail.com>
2014-07-10 15:56:09 +0200

    3 files added

--- implementation/webservice/ws/Gassing.php
+++ implementation/webservice/ws/Gassing.php
@@ -41,7 +41,77 @@ class Gassing extends Scene{
         $paraObjAll =new gassingParameters();
         
         $GassingCases = array();
+        
+        if($formData == NULL)
+        {
+            parent::__construct(null,null,"","","","","","","",null,$api);
+        }else {
+            for($i = 0; $i < count($formData['object']);$i++)
+            {
+                parent::__construct($formData['object'][$i]['sceneTime'],"Burn",$formData['object'][$i]['sceneDate'],$formData['object'][$i]['sceneLocation'],$formData['object'][$i]['sceneTemparature']
+                        ,$formData['object'][$i]['investigatingOfficerName'],$formData['object'][$i]['investigatingOfficerRank'],$formData['object'][$i]['investigatingOfficerCellNo'],$formData['object'][$i]['firstOfficerOnSceneName'],$formData['object'][$i]['firstOfficerOnSceneRank'],$api);
+                $this->gassingIOType = $formData['object'][$i]['gassingIOType'];
+                $this->signsOfStruggle = $formData['object'][$i]['signsOfStruggle'];
+                $this->alcoholBottleAround = $formData['object'][$i]['alcoholBottleAround'];
+                $this->drugParaphernalia = $formData['object'][$i]['drugParaphernalia'];
+                
+               $sceneID = $this->createScene();
+                 if($sceneID == NULL){
+                     $error = array('status' => "Failed", "msg" => "Request to create a scene was denied.");
+                     $this->api->response($this->api->json($error), 400);
+                 }
+                $this->setVictim($sceneID,$formData['object'][$i]['victims']);
+                $this->setCase($sceneID, $formData['object'][$i]['FOPersonelNumber']);
+                if($formData['object'][$i]['victims']['victimInside'] == "yes"){
+                    $this->addGassing($sceneID,TRUE,$formData['object'][$i]);
+                }else{
+                    $this->addGassing($sceneID,FALSE,null);
+                }
+            }
+            
+            
+        }
 	
+    }
+    
+    public function addGassing($sceneID,$inside,$object) {
+        
+        $h_res = mysql_query("insert into gassing values(0,".$sceneID.",'$this->gassingIOType','$this->signsOfStruggle','$this->alcoholBottleAround','$this->drugParaphernalia')");
+        if($h_res == FALSE){
+            $error = array('status' => "Failed", "msg" => "Request to create a scene was denied.");
+            $this->api->response($this->api->json($error), 400);
+        }
+        
+        if($inside == TRUE){
+            $h_res = mysql_query("select gassingID from gassing where sceneID=".$sceneID);
+            $gassingID = mysql_result($h_res,0,'gassingID');
+            $dl = $object['doorLocked'];
+            $wc = $object['windowsClosed'];
+            $wb = $object['windowsBroken'];
+            $va = $object['victimAlone'];
+            $pv = $object['peopleWithVictim'];
+            $ga = $object['gassingAppliances']; 
+            $gau = $object['gassingAppliancesUsed'];
+            $gs = $object['gassingSmell'];
+            if($va != "yes")
+            {
+                $hi_res = mysql_query("insert into gassinginside values(0,".$gassingID.",'$dl','$wc','$wb','$va','$pv','$ga','$gau','$gs')");
+            }else{
+                $hi_res = mysql_query("insert into gassinginside values(0,".$gassingID.",'$dl','$wc','$wb','$va',null','$ga','$gau','$gs)");
+            }
+        }else{
+            
+            $h_res = mysql_query("select gassingID from gassing where sceneID=".$sceneID);
+            $gassingID = mysql_result($h_res,0,'gassingID');
+            $gvc = $object['gassingVictimInCar'];
+            $vcd = $object['victimInCarDescription'];
+            
+           
+           $hi_res = mysql_query("insert into gassingoutside values(0,".$gassingID.",'$gvc','$vcd')");
+           
+            
+        }
+        
     }
     
     public function getAllGassingCases() {
--- implementation/webservice/ws/IngestionOverdosePoisoning.php
+++ implementation/webservice/ws/IngestionOverdosePoisoning.php
@@ -37,7 +37,64 @@ class IngestionOverdosePoisoning extends Scene{
         $paraObjAll =new ingestionParameters();
         
         $ingestionCases = array();
+        
+        if($formData == NULL)
+        {
+            parent::__construct(null,null,"","","","","","","",null,$api);
+        }else {
+            for($i = 0; $i < count($formData['object']);$i++)
+            {
+                parent::__construct($formData['object'][$i]['sceneTime'],"Burn",$formData['object'][$i]['sceneDate'],$formData['object'][$i]['sceneLocation'],$formData['object'][$i]['sceneTemparature']
+                        ,$formData['object'][$i]['investigatingOfficerName'],$formData['object'][$i]['investigatingOfficerRank'],$formData['object'][$i]['investigatingOfficerCellNo'],$formData['object'][$i]['firstOfficerOnSceneName'],$formData['object'][$i]['firstOfficerOnSceneRank'],$api);
+                $this->ingestionOverdosePoisoningIOType = $formData['object'][$i]['ingestionOverdosePoisoningIOType'];
+                $this->signsOfStruggle = $formData['object'][$i]['signsOfStruggle'];
+                $this->alcoholBottleAround = $formData['object'][$i]['alcoholBottleAround'];
+                $this->drugParaphernalia = $formData['object'][$i]['drugParaphernalia'];
+                
+               $sceneID = $this->createScene();
+                 if($sceneID == NULL){
+                     $error = array('status' => "Failed", "msg" => "Request to create a scene was denied.");
+                     $this->api->response($this->api->json($error), 400);
+                 }
+                $this->setVictim($sceneID,$formData['object'][$i]['victims']);
+                $this->setCase($sceneID, $formData['object'][$i]['FOPersonelNumber']);
+                if($formData['object'][$i]['victims']['victimInside'] == "yes"){
+                    $this->addIngestionOverdosePoisoning($sceneID,TRUE,$formData['object'][$i]);
+                }else{
+                    $this->addIngestionOverdosePoisoning($sceneID,FALSE,null);
+                }
+            }
+            
+            
+        }
 	
+    }
+    
+    public function addIngestionOverdosePoisoning($sceneID,$inside,$object) {
+        
+        $h_res = mysql_query("insert into ingestionoverdosepoisoning values(0,".$sceneID.",'$this->ingestionOverdosePoisoningIOType','$this->signsOfStruggle','$this->alcoholBottleAround','$this->drugParaphernalia')");
+        if($h_res == FALSE){
+            $error = array('status' => "Failed", "msg" => "Request to create a scene was denied.");
+            $this->api->response($this->api->json($error), 400);
+        }
+        
+        if($inside == TRUE){
+            $h_res = mysql_query("select ingestionOverdosePoisoningID from ingestionoverdosepoisoning where sceneID=".$sceneID);
+            $ingestionOverdosePoisoningID = mysql_result($h_res,0,'ingestionOverdosePoisoningID');
+            $dl = $object['doorLocked'];
+            $wc = $object['windowsClosed'];
+            $wb = $object['windowsBroken'];
+            $va = $object['victimAlone'];
+            $pv = $object['peopleWithVictim'];
+            
+            if($va != "yes")
+            {
+                $hi_res = mysql_query("insert into ingestionoverdosepoisoninginside values(0,".$ingestionOverdosePoisoningID.",'$dl','$wc','$wb','$va','$pv')");
+            }else{
+                $hi_res = mysql_query("insert into ingestionoverdosepoisoninginside values(0,".$ingestionOverdosePoisoningID.",'$dl','$wc','$wb','$va',null)");
+            }
+        }
+        
     }
     
     public function getAllIngestionOverdosePoisoningCases() {
