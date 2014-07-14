5c9a701f82a9498b352043c5232497f19e7daa73
legodi-301 <u29302732@tuks.co.za>
2014-07-14 14:22:33 +0200

    https setup

--- implementation/webservice/ws/controls/script.js
+++ implementation/webservice/ws/controls/script.js
@@ -98,7 +98,7 @@ function sendRequest(data){
             alert("error: "+result);
         }
     }).responseText;*/
-    request.open("POST","models/api.php");
+    request.open("POST","../models/api.php");
     
     request.send(data);
     
--- implementation/webservice/ws/index.php
+++ implementation/webservice/ws/index.php
@@ -1,32 +1,15 @@
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Forensics</title>
-        <link rel="stylesheet" type="text/css" href="models/css/style.css"/>
-        <script type="text/javascript" src="controls/jquery.js"></script>
-        <script type="text/javascript" src="controls/script.js"></script>
-    </head>
-    <body>
-        <div id="header">
-            <header class="header-content">
-                
-            </header>
-        </div>
-        <div id="login-content">
-            <div id="login-form">
-                <label for="username">
-                    Username:<br><input class="login-form-input" type="text" name="username" id="username" placeholder="personel number" /><br><br>
-                </label>
-                <label for="password">
-                    Password:<br><input class="login-form-input" type="password" name="password" id="password" placeholder="password" /><br><br>
-                </label>
-                <button id="login-form-button">Login</button>
-            </div>
-        </div>
-        <div id="footer">
-            &copy; Copyrighted 2014
-        </div>
-    </body>
-</html>
+<?php
+    if(('on' == $_SERVER['HTTPS']))
+	{
+		$uri = 'https://';
+	}else{
+		try{
+			$_SERVER['HTTPS'] = 'on';
+			$uri = 'https://';
+		}catch(Exception $exc){
+			$uri = 'http://';
+		}
+	}
+        $uri .= $_SERVER['HTTP_HOST'];
+        header('Location: '.$uri.'/ws/views/');
+?>
