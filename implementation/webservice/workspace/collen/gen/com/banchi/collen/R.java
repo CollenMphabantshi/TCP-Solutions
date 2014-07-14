aedde3495836e884969b01a20ee97f41b56a01fb
legodi-301 <u29302732@tuks.co.za>
2014-07-14 17:09:04 +0200

    https setup

--- implementation/webservice/workspace/collen/gen/com/banchi/collen/R.java
+++ implementation/webservice/workspace/collen/gen/com/banchi/collen/R.java
@@ -25,14 +25,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int linearLayout1=0x7f080001;
-        public static final int scrollView1=0x7f080000;
-        public static final int textview=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int logginId=0x7f080003;
+        public static final int passwordId=0x7f080001;
+        public static final int rememberMeId=0x7f080002;
+        public static final int responseTxt=0x7f080004;
+        public static final int usernameId=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
